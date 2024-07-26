8f4efb49e32e0f972ec8b595cdf954c86f8e5a35
david.simonovic <davidsimonovicdev@gmail.com>
2024-07-26 17:00:54 +0200

    Site_id

--- routes/web.php
+++ routes/web.php
@@ -122,9 +122,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/testNumbers', function () {
 
         $numbers = [
-            ['ad_title' => 'David', 'city' => 'Negotin', 'postcode' => '11111', 'number' => '+38163391116','site_id' => 99,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
-            ['ad_title' => 'Chris', 'city' => 'Berlin', 'postcode' => '11111', 'number' => '01747347642','site_id' => 99,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
-            ['ad_title' => 'Bart', 'city' => 'Merl', 'postcode' => '11111', 'number' => '+491774013569', 'site_id' => 99,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
+            ['ad_title' => 'David', 'city' => 'Negotin', 'postcode' => '11111', 'number' => '+38163391116','site_id' => 6,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
+            ['ad_title' => 'Chris', 'city' => 'Berlin', 'postcode' => '11111', 'number' => '01747347642','site_id' => 6,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
+            ['ad_title' => 'Bart', 'city' => 'Merl', 'postcode' => '11111', 'number' => '+491774013569', 'site_id' => 6,'active' => 1,'bounced' => 0, 'url_id' => 110010001],
         ];
 
         foreach ($numbers as $number)
